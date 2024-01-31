fd5f7b183b3f03be7267785e00843313460c57c4
douglasbarreiros701 <douglasb701@gmail.com>
2024-01-31 20:15:29 -0300

    first commit

---
+++ src/main/java/com/aLivraria/api/ALivrariaApplication.java
@@ -0,0 +1,13 @@
+package com.aLivraria.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ALivrariaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ALivrariaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aLivraria/api/ALivrariaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aLivraria.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ALivrariaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
